14226d3f523a6d4bb584d7e467692d3bcd47d040
Naz9898 <nazmul.hossain.iic.98@gmail.com>
2023-08-06 19:22:02 +0200

    New callback function to pass query results

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,40 @@
 var express = require('express');
+var sqlite3 = require('sqlite3');
 var router = express.Router();
+
+
+var data = [
+  {
+    id: 0,
+    title: "Articolo 0",
+    content: "Bla bla bla"
+  },
+  {
+    id: 1,
+    title: "Articolo 1",
+    content: "Bla bla bla"
+  }
+];
+
+function runQueries(cb) {
+  var db = new sqlite3.Database('blog.db');
+  res = [];
+  db.all(`select * from article`, (err, rows) => {
+      rows.forEach(row => {
+          res.push({id: row.id, title:row.title, content:row.content});
+      });
+      console.log(res);
+      return cb(res);
+  });
+  
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'NazBlog' });
-
+  runQueries((articles) => {
+    console.log("Query finished!");
+    res.render('index', { title: 'NazBlog', articles: data});
+  });
 });
 
 module.exports = router;
